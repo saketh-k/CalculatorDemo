33c080831ce2b0a8510d37650ad19d0c90d6d295
Saketh Karumuri <skarumur@uci.edu>
2022-08-04 19:45:46 -0700

    Created framework of calculator

---
+++ calc.py
@@ -0,0 +1,33 @@
+from argparse import ArgumentError
+import sys
+
+def add(x, y):
+    pass
+
+def sub(x, y):
+    pass
+
+def mult(x, y):
+    pass
+
+def div(x, y):
+    pass
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        raise ArgumentError("Incorrect Number of Arguments")
+    
+    x = sys.argv[1]
+    y = sys.argv[2]
+
+    if sys.argv[0].lower() == "add":
+        print(add(x, y))
+
+    if sys.argv[0].lower() == "sub":
+        print(sub(x, y))
+
+    if sys.argv[0].lower() == "mult":
+        print(mult(x, y))
+
+    if sys.argv[0].lower() == "div":
+        print(div(x, y))
